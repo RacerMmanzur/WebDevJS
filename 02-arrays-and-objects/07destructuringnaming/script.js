93139886726f9da3736b686f3a3252854ffada90
RacerMmanzur <senpaimiggy18@gmail.com>
2023-09-26 10:09:49 +0800

    main

--- 02-arrays-and-objects/07destructuringnaming/script.js
+++ 02-arrays-and-objects/07destructuringnaming/script.js
@@ -13,17 +13,17 @@
 // console.log(person.age);
 // output: 30 (age)
 
-const firstName = 'Juan';
-const lastName = 'Dela Cruz';
-const age = 30;
+// const firstName = 'Juan';
+// const lastName = 'Dela Cruz';
+// const age = 30;
 
-const person = {
-  firstName,
-  lastName,
-  age,
-};
+// const person = {
+//   firstName,
+//   lastName,
+//   age,
+// };
 
-console.log(person.age);
+// console.log(person.age);
 
 // Destructuring the code above
 // const todo = {
@@ -38,13 +38,37 @@ console.log(person.age);
 // console.log(id);
 
 //more
-const todo = {
-  id: 1,
-  title: 'magluto ng Scrammble Egg',
-  user: {
-    name: 'Juan',
-  },
-};
+// const todo = {
+//   id: 1,
+//   title: 'magluto ng Scrammble Egg',
+//   user: {
+//     name: 'Juan',
+//   },
+// };
 
-const { id, title } = todo.id;
-console.log(id, title);
+// const { id, title } = todo.id;
+// console.log(id, title);
+
+// const todo = {
+//   id: 1,
+//   title: 'magluto ng Scrammble Egg',
+//   user: {
+//     name: 'Juan',
+//   },
+// };
+
+// const todo = {
+//   id: todoId,
+//   title,
+//   user: { name },
+// } = todo;
+
+// console.log(todoId);
+
+const numbers = [23, 67, 33, 49, 52];
+
+// const[first, second] = numbers;
+// console.log(first, second);
+
+const [first, second, ...rest] = numbers;
+console.log(first, second, rest);
