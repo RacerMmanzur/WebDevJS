3e2de4169e14c2af13225e06ef59a23d7e166d2d
RacerMmanzur <senpaimiggy18@gmail.com>
2023-09-27 09:56:54 +0800

    main

--- 03-functions-scope/09challenge/script.js
+++ 03-functions-scope/09challenge/script.js
@@ -1,8 +1,10 @@
+//###############################################
 //challenge 1 solution
 const getCelsius = (fahrenheit) => (fahrenheit - 32) * (5 / 9);
 
 console.log(`The temperature is ${getCelsius(32)} \xB0C`);
 
+//###############################################
 //challenge 2 solution
 const minMax = (num) => ({
   min: Math.min(...num),
@@ -11,10 +13,11 @@ const minMax = (num) => ({
 
 console.log(minMax([1, 2, 3, 4, 5]));
 
+//###############################################
 //challenge 3 solution
 (function () {
-  const length = 45;
-  const width = 7;
+  const length = 10;
+  const width = 5;
   const area = length * width;
 
   console.log(
